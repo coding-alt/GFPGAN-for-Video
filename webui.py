d2cd61db6dcab2c9485160ce17f29c6638ad3d63
颜玉刚 <yanyugang@gmail.com>
2024-12-10 17:26:01 +0800

    Update webui.py

--- webui.py
+++ webui.py
@@ -1,28 +1,39 @@
 import gradio as gr
-from utils.gfpgan_wrapper import VideoEnhancer
+from utils.gfpgan_wrapper import GfpganWrapper
 import asyncio
 
-video_enhancer = VideoEnhancer()
+gfpgan_wrapper = GfpganWrapper()
 
 async def enhance_video(video, batch_size):
     print(f"Enhancing video: {video}")
-    output_path = await video_enhancer.video_enhance(video, batch_size)
+    output_path = await gfpgan_wrapper.video_enhance(video, batch_size)
+    print(f"Output path: {output_path}")
+    return output_path
+
+async def enhance_video(image):
+    print(f"Enhancing image: {image}")
+    output_path = await gfpgan_wrapper.image_enhance(image)
     print(f"Output path: {output_path}")
     return output_path
 
 css = """footer {visibility: hidden}"""
 with gr.Blocks(title="视频高清化", css=css, theme=gr.themes.Soft(primary_hue=gr.themes.colors.sky)) as demo:
-    gr.Markdown("# 视频高清化")
-    gr.Markdown("上传一个视频以增强其质量。")
-    
-    with gr.Row():
-        video_input = gr.Video(label="上传视频")
-        video_output = gr.Video(label="高清化后的视频")
-    
-    with gr.Row():
-        batch_size = gr.Slider(label="批量大小", minimum=1, maximum=256, value=128, step=1)
-        btn = gr.Button("一键高清")
+    gr.Markdown("# 视频和图片高清化")
 
-    btn.click(fn=enhance_video, inputs=[video_input, batch_size], outputs=video_output)
+    with gr.Tab("视频高清化"):
+        gr.Markdown("上传一个视频以增强其质量。")
+        with gr.Row():
+            video_input = gr.Video(label="上传视频")
+            video_output = gr.Video(label="高清化后的视频")
+        btn_video = gr.Button("一键高清")
+        btn_video.click(enhance_video, inputs=video_input, outputs=video_output)
+
+    with gr.Tab("图片高清化"):
+        gr.Markdown("上传一张图片以增强其质量。")
+        with gr.Row():
+            image_input = gr.Image(label="上传图片", type="filepath")
+            image_output = gr.Image(label="高清化后的图片")
+        btn_image = gr.Button("一键高清")
+        btn_image.click(enhance_image, inputs=image_input, outputs=image_output)
 
 demo.launch(server_name="0.0.0.0")
